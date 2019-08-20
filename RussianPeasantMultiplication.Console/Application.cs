14c649b32b4b6c4542924ad36233f2415235830e
Henrik Ilgen <henrik.ilgen@acando.de>
2019-08-20 20:19:05 +0200

    Added a console application to use multiplier

---
+++ RussianPeasantMultiplication.Console/Application.cs
@@ -0,0 +1,31 @@
+﻿using RussianPeasantMultiplication.Calculation;
+using RussianPeasantMultiplication.Formatting;
+
+namespace RussianPeasantMultiplication.Console {
+    /// <summary>
+    /// Provides a more convenient entry point for the application.
+    /// This class supports constructor dependency injection and
+    /// offers a single <see cref="Run"/> method that's invoked
+    /// with the parameters passed on the command line.
+    /// </summary>
+    class Application {
+        private readonly IMultiplier multiplier;
+        private readonly IMultiplicationResultFormatter formatter;
+
+        public Application(
+            IMultiplier multiplier,
+            IMultiplicationResultFormatter formatter
+        ) {
+            this.multiplier = multiplier;
+            this.formatter = formatter;
+        }
+
+        internal void Run(int left, int right) {
+            System.Console.WriteLine(
+                formatter.FormatToString(
+                    multiplier.Multiply(left, right)
+                )
+            );
+        }
+    }
+}
---
+++ RussianPeasantMultiplication.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RussianPeasantMultiplication.Calculation;
+using RussianPeasantMultiplication.Formatting;
+
+namespace RussianPeasantMultiplication.Console {
+    /// <summary>
+    /// The main entry point for the application.
+    /// This project uses the DragonFruit application model
+    /// (https://github.com/dotnet/command-line-api/wiki/DragonFruit-overview)
+    /// in order to provide a strongly-types main method.
+    /// </summary>
+    class Program {
+        /// <summary>
+        /// Multiplies two numbers via the Russian Peasant Multiplication algorithm.
+        /// This program will output the steps performed during the calculation.
+        /// </summary>
+        /// <param name="left">The first operand.</param>
+        /// <param name="right">The second operand.</param>
+        static void Main(int left, int right) {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            serviceCollection.BuildServiceProvider()
+                .GetRequiredService<Application>()
+                .Run(left, right);
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+            => serviceCollection
+                .AddTransient<IMultiplier, RussianPeasantMultiplier>()
+                .AddTransient<IMultiplicationResultFormatter, MultiplicationResultFormatter>()
+                .AddSingleton<Application>()
+            ;
+    }
+}
